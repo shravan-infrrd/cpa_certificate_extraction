f714ac2461857b76dccc573f0814cf463d14b0af
Ubuntu <ubuntu@ip-172-31-25-172.us-west-2.compute.internal>
2019-08-16 07:20:00 +0000

    mongo changes

--- test/service/extract_fields.py
+++ test/service/extract_fields.py
@@ -11,9 +11,7 @@ from bson.objectid import ObjectId
 from constant import MONGO_URI
 #from log import get_logger
 
-client = MongoClient(MONGO_URI)
-database = client.cpa_database
-certificates = database.certificates
+
 
 class Encoder(json.JSONEncoder):
     def default(self, obj):
@@ -23,16 +21,26 @@ class Encoder(json.JSONEncoder):
             return obj
 
 
+def connect_to_db():
+		client = MongoClient(MONGO_URI)
+		database = client.cpa_database
+		certificates = database.certificates
+		return certificates
 
 def fetch_data_from_mongo(doc_id=None):
-		if doc_id is None:
-				documents = certificates.find()
-				email_data_lists = []
-				for doc in documents:
-						email_data_lists.append({'_id': str(doc['_id']), 'file_name': doc['pdf_file_path'].split('/')[-1] })
-				return email_data_lists
-		else:
-				document = certificates.find_one({'_id': ObjectId(doc_id) })
-				data = {'file_name': document['pdf_file_path']}
-				return json.dumps(document, cls=Encoder)
+		try:
+				certificates = connect_to_db()
+				if doc_id is None:
+						documents = certificates.find()
+						email_data_lists = []
+						for doc in documents:
+								email_data_lists.append({'_id': str(doc['_id']), 'file_name': doc['pdf_file_path'].split('/')[-1] })
+						return email_data_lists
+				else:
+						document = certificates.find_one({'_id': ObjectId(doc_id) })
+						data = {'file_name': document['pdf_file_path']}
+						return json.dumps(document, cls=Encoder)
+		except:
+				
+				pass
 
